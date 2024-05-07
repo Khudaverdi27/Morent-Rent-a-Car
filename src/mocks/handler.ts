f8e5e94235add73940b1d57ca783b5c0908ac59c
Xudaverdi <xudaverdi.sh@gmail.com>
2024-05-07 23:58:16 +0400

    created:DB for cars

--- src/mocks/handler.ts
+++ src/mocks/handler.ts
@@ -1,13 +1,10 @@
 import { http, HttpResponse } from "msw";
+import data from "../data/banner.json";
 
 export const handlers = [
   // Intercept "GET https://example.com/user" requests...
   http.get("https://example.com/user", () => {
     // ...and respond to them using this JSON response.
-    return HttpResponse.json({
-      id: "c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d",
-      firstName: "John",
-      lastName: "Maverick",
-    });
+    return HttpResponse.json(data);
   }),
 ];
