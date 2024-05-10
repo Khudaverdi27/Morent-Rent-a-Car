8a5038a1e5fda9020341add07ffdef5207229770
Xudaverdi <xudaverdi.sh@gmail.com>
2024-05-11 01:21:37 +0400

    added: loading to navbar

--- src/Form/Search.tsx
+++ src/Form/Search.tsx
@@ -4,34 +4,62 @@ import {
   InputGroup,
   InputLeftElement,
   InputRightElement,
+  Skeleton,
+  SkeletonCircle,
 } from "@chakra-ui/react";
 import { CiSearch } from "react-icons/ci";
 import { GiSettingsKnobs } from "react-icons/gi";
+import { useGetByPopularQuery } from "../services/request";
 
 function Search() {
+  const { isLoading } = useGetByPopularQuery("banner");
   return (
     <InputGroup>
       <InputLeftElement pointerEvents="none">
-        <CiSearch color="Secondary.400" />
+        {isLoading ? (
+          <SkeletonCircle
+            startColor="Primary.300"
+            endColor="Primary.200"
+            size="4"
+          />
+        ) : (
+          <CiSearch color="Secondary.400" />
+        )}
       </InputLeftElement>
+
       <Input
         borderRadius={"3xl"}
         type="text"
-        placeholder="Search something here"
+        placeholder={isLoading ? "" : "Search something here"}
         _placeholder={{ fontSize: "14px" }}
       />
       <InputRightElement width="4.5rem">
-        <Button
-          _hover={{
-            background: "transparent",
-          }}
-          variant="ghost"
-          h="1.75rem"
-          size="sm"
-        >
-          <GiSettingsKnobs color="Secondary.400" style={{ rotate: "90deg" }} />
-        </Button>
+        {!isLoading && (
+          <Button
+            _hover={{
+              background: "transparent",
+            }}
+            variant="ghost"
+            h="1.75rem"
+            size="sm"
+          >
+            <GiSettingsKnobs
+              color="Secondary.400"
+              style={{ rotate: "90deg" }}
+            />
+          </Button>
+        )}
       </InputRightElement>
+      {isLoading && (
+        <Skeleton
+          top={"32%"}
+          left={"8%"}
+          pos={"absolute"}
+          borderRadius={"10px"}
+          w={115}
+          height="15px"
+        />
+      )}
     </InputGroup>
   );
 }
--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Flex, HStack, Spacer } from "@chakra-ui/react";
+import { Avatar, Flex, HStack, Skeleton, Spacer } from "@chakra-ui/react";
 import { FaHeart } from "react-icons/fa6";
 import { BiSolidBell } from "react-icons/bi";
 import { IoMdSettings } from "react-icons/io";
@@ -6,8 +6,11 @@ import { Icon } from "@chakra-ui/react";
 import Logo from "./Logo";
 import Search from "../../Form/Search";
 import IconBtn from "../shared/IconBtn";
+import { useGetByPopularQuery } from "../../services/request";
 
 function Navbar() {
+  const { isLoading } = useGetByPopularQuery("banner");
+
   return (
     <Flex
       zIndex={"10"}
@@ -23,22 +26,51 @@ function Navbar() {
       as="header"
     >
       <HStack w={"50%"} spacing="54px">
-        <Logo />
+        {isLoading ? (
+          <Skeleton
+            startColor="Primary.300"
+            endColor="Primary.200"
+            borderRadius={"10px"}
+            w={200}
+            height="16px"
+          />
+        ) : (
+          <Logo />
+        )}
+
         <Search />
       </HStack>
       <Spacer />
       <HStack spacing="14px">
         <IconBtn>
-          <Icon as={FaHeart} boxSize={5} />
+          <Icon
+            color={isLoading ? "Primary.300" : ""}
+            as={FaHeart}
+            boxSize={5}
+          />
         </IconBtn>
         <IconBtn>
-          <Icon className="bell--icon" as={BiSolidBell} boxSize={5} />
+          <Icon
+            color={isLoading ? "Primary.300" : ""}
+            className="bell--icon"
+            as={BiSolidBell}
+            boxSize={5}
+          />
         </IconBtn>
         <IconBtn>
-          <Icon as={IoMdSettings} boxSize={5} />
+          <Icon
+            color={isLoading ? "Primary.300" : ""}
+            as={IoMdSettings}
+            boxSize={5}
+          />
         </IconBtn>
         <IconBtn>
-          <Avatar w={"44px"} h={"44px"} src="#" />
+          <Avatar
+            bgColor={isLoading ? "Primary.300" : ""}
+            w={"44px"}
+            h={"44px"}
+            src="#"
+          />
         </IconBtn>
       </HStack>
     </Flex>
